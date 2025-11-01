b247b2fab1007ab5b7a58ce87167e8ef733d4fb7
thillaiyadiking <thillaiyadiking@gmail.com>
2025-11-01 22:13:40 +0530

    `Update index.php: change ENVIRONMENT from 'development' to 'production'`,added warehouse, manufacturing and purchase module

---
+++ application/logs/log-2025-11-01.php
@@ -0,0 +1,209 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-11-01 14:45:37 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:45:37 --> Could not find the language line "setting"
+ERROR - 2025-11-01 14:45:37 --> Query error: Unknown column 'parent_id' in 'field list' - Invalid query: SELECT *
+FROM `tblitems`
+WHERE tblitems.id not in ( SELECT distinct parent_id from tblitems WHERE parent_id is not null AND `parent_id` != "0" )
+ERROR - 2025-11-01 14:45:47 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:45:47 --> Could not find the language line "setting"
+ERROR - 2025-11-01 14:45:47 --> Could not find the language line "setting"
+ERROR - 2025-11-01 11:45:48 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:48 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:48 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:48 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:48 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:48 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:48 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:45:51 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:45:51 --> Could not find the language line "setting"
+ERROR - 2025-11-01 14:45:51 --> Could not find the language line "dashboard"
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:45:52 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:45:55 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:45:55 --> Could not find the language line "setting"
+ERROR - 2025-11-01 14:45:55 --> Query error: Unknown column 'parent_id' in 'field list' - Invalid query: SELECT *
+FROM `tblitems`
+WHERE tblitems.id not in ( SELECT distinct parent_id from tblitems WHERE parent_id is not null AND `parent_id` != "0" )
+ERROR - 2025-11-01 14:52:20 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:52:20 --> Could not find the language line "setting"
+ERROR - 2025-11-01 11:52:20 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:52:20 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:52:20 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:52:20 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:52:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:52:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:52:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:52:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:52:24 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:52:24 --> Could not find the language line "setting"
+ERROR - 2025-11-01 14:52:24 --> Query error: Unknown column 'tblitems.active' in 'where clause' - Invalid query: SELECT *
+FROM `tblitems`
+WHERE `tblitems`.`active` = 1 AND `tblitems`.`parent_id` is not null AND  `tblitems`.`parent_id` !=0 AND  `tblitems`.`attributes` is not null
+ERROR - 2025-11-01 14:53:12 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:53:12 --> Could not find the language line "setting"
+ERROR - 2025-11-01 14:53:12 --> Query error: Unknown column 'tblitems.attributes' in 'where clause' - Invalid query: SELECT *
+FROM `tblitems`
+WHERE `tblitems`.`active` = 1 AND `tblitems`.`parent_id` is not null AND  `tblitems`.`parent_id` !=0 AND  `tblitems`.`attributes` is not null
+ERROR - 2025-11-01 14:53:14 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:53:14 --> Could not find the language line "setting"
+ERROR - 2025-11-01 14:53:14 --> Query error: Unknown column 'tblitems.attributes' in 'where clause' - Invalid query: SELECT *
+FROM `tblitems`
+WHERE `tblitems`.`active` = 1 AND `tblitems`.`parent_id` is not null AND  `tblitems`.`parent_id` !=0 AND  `tblitems`.`attributes` is not null
+ERROR - 2025-11-01 14:53:15 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:53:15 --> Could not find the language line "setting"
+ERROR - 2025-11-01 14:53:16 --> Query error: Unknown column 'tblitems.attributes' in 'where clause' - Invalid query: SELECT *
+FROM `tblitems`
+WHERE `tblitems`.`active` = 1 AND `tblitems`.`parent_id` is not null AND  `tblitems`.`parent_id` !=0 AND  `tblitems`.`attributes` is not null
+ERROR - 2025-11-01 14:53:41 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:53:41 --> Could not find the language line "setting"
+ERROR - 2025-11-01 11:53:41 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:41 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:41 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:41 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:41 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:41 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:41 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:41 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:53:43 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:53:43 --> Could not find the language line "setting"
+ERROR - 2025-11-01 11:53:44 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:44 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:44 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:44 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:44 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:44 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:44 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:44 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:53:56 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:53:56 --> Could not find the language line "setting"
+ERROR - 2025-11-01 11:53:56 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:56 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:56 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:56 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:56 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:56 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:53:56 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:55:25 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:55:25 --> Could not find the language line "setting"
+ERROR - 2025-11-01 11:55:25 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:25 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:25 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:25 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:26 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:26 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:26 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:26 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:55:30 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:55:30 --> Could not find the language line "setting"
+ERROR - 2025-11-01 11:55:30 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:30 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:30 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:31 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:31 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:31 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:55:31 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:55:35 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:55:35 --> Could not find the language line "setting"
+ERROR - 2025-11-01 14:55:35 --> Severity: Notice --> Undefined variable: b_id D:\laragon\www\Ali\erp_aktc_site\application\controllers\admin\Mods.php 29
+ERROR - 2025-11-01 14:55:35 --> Severity: Notice --> Undefined variable: bid D:\laragon\www\Ali\erp_aktc_site\application\libraries\App_modules.php 77
+ERROR - 2025-11-01 14:56:13 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 11:56:13 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:13 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:13 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:13 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:13 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:13 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:13 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:56:16 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 11:56:16 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:16 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:16 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:16 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:17 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:17 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:17 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:17 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:56:21 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 11:56:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:21 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:56:27 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:56:27 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 526
+ERROR - 2025-11-01 14:56:27 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 527
+ERROR - 2025-11-01 14:56:27 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 538
+ERROR - 2025-11-01 14:56:27 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 539
+ERROR - 2025-11-01 14:56:27 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 541
+ERROR - 2025-11-01 14:56:27 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 542
+ERROR - 2025-11-01 14:56:27 --> Could not find the language line "List items"
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:27 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:56:28 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:56:28 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 526
+ERROR - 2025-11-01 14:56:28 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 527
+ERROR - 2025-11-01 14:56:28 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 538
+ERROR - 2025-11-01 14:56:28 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 539
+ERROR - 2025-11-01 14:56:28 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 541
+ERROR - 2025-11-01 14:56:28 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 542
+ERROR - 2025-11-01 14:56:35 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:56:35 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 526
+ERROR - 2025-11-01 14:56:35 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 527
+ERROR - 2025-11-01 14:56:35 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 538
+ERROR - 2025-11-01 14:56:35 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 539
+ERROR - 2025-11-01 14:56:35 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 541
+ERROR - 2025-11-01 14:56:35 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 542
+ERROR - 2025-11-01 14:56:35 --> Could not find the language line "Vendor-Items"
+ERROR - 2025-11-01 11:56:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 11:56:36 --> 404 Page Not Found: /index
+ERROR - 2025-11-01 14:56:36 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:56:36 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 526
+ERROR - 2025-11-01 14:56:36 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 527
+ERROR - 2025-11-01 14:56:36 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 538
+ERROR - 2025-11-01 14:56:36 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 539
+ERROR - 2025-11-01 14:56:36 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 541
+ERROR - 2025-11-01 14:56:36 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 542
+ERROR - 2025-11-01 14:56:46 --> Could not find the language line "E-Sign"
+ERROR - 2025-11-01 14:56:46 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 526
+ERROR - 2025-11-01 14:56:46 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 527
+ERROR - 2025-11-01 14:56:46 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 538
+ERROR - 2025-11-01 14:56:46 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 539
+ERROR - 2025-11-01 14:56:46 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 541
+ERROR - 2025-11-01 14:56:46 --> Severity: 8192 --> Array and string offset access syntax with curly braces is deprecated D:\laragon\www\Ali\erp_aktc_site\modules\purchase\third_party\excel\PHPExcel\Shared\String.php 542
+ERROR - 2025-11-01 14:56:46 --> Could not find the language line "Vendor-Items"
